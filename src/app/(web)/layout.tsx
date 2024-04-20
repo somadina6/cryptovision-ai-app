5cc945fc892d1f107790ad795a01d9f1817120e5
somadina6 <somadina6@gmail.com>
2024-04-19 22:41:01 -0300

    update

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider session={session}>
+        <NextAuthProvider>
           <UserProvider>
             <ThemeProvider>
               <Toast />
