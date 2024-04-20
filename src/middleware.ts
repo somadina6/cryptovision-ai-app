c65c9c76ac57aa32daa2aee74d50ecc620d684f1
somadina6 <somadina6@gmail.com>
2024-04-19 21:41:02 -0300

    update csrf issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 // export { default } from "next-auth/middleware";
-// import { withAuth } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-// export default withAuth(
-//   // `withAuth` augments your `Request` with the user's token.
-//   function middleware(req) {},
-//   {
-//     pages: {
-//       signIn: "/auth/login",
-//       error: "/auth/error",
-//       newUser: "/auth/signup",
-//     },
-//   }
-// );
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {},
+  {
+    pages: {
+      signIn: "/auth/login",
+      error: "/auth/error",
+      newUser: "/auth/signup",
+    },
+  }
+);
 
-// export const config = { matcher: ["/api/token/:path*"] };
+export const config = { matcher: ["/api/token/:path*"] };
--- src/utils/api.ts
+++
@@ -1,7 +0,0 @@
-import { GetServerSidePropsContext } from "next";
-import { getCsrfToken } from "next-auth/react";
-
-export async function getCsrfToken_(context: GetServerSidePropsContext) {
-  const csrfToken = await getCsrfToken(context);
-  return csrfToken;
-}
